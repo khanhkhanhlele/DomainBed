8b1a4f30dc8eff80de24319a905cd620aa500a7c
khanhkhanhlele <namkhanh2172@gmail.com>
2024-05-08 22:01:55 +0700

    x

---
+++ push_wandb.py
@@ -0,0 +1,14 @@
+import wandb
+import os
+wandb.login(key="1eac4d04cc3cc4aed9a1409cd8eb7dc0f6537ef2")
+
+out_dir = "./train_output/fff0210ead18835154d2bc2feb72344b" 
+run_name = "CAG1_stop_early_" + os.path.basename(out_dir)
+path1 = os.path.join(out_dir, 'results.jsonl')
+path2 = os.path.join(out_dir, 'out.txt')
+wandb.init(project="DomainBed3",
+        entity="namkhanh2172",
+        name=run_name,
+        force=True)
+wandb.save(path1)
+wandb.save(path2)
